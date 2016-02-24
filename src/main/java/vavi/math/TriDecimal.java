ea777965d4819586952cc7ec741a2d9175c91847
Naohide Sano <sano-n@klab.com>
2016-02-24 17:11:02 +0800

    add n decimal classes

---
+++ src/main/java/vavi/math/TriDecimal.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.math;
+
+
+/**
+ * TriDecimal. 
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2015/11/06 umjammer initial version <br>
+ */
+public class TriDecimal extends NDecimal {
+
+    public TriDecimal(int value) {
+        super(value, 3);
+    }
+
+    public static int toDecimal(String nDecimal) {
+        return toDecimal(nDecimal, 3);
+    }
+}
+
+/* */
